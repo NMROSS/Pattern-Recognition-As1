af6597a466a397ed5244e05b0df628dcff8cf720
nmross <nmross@mail.com>
2021-05-21 06:10:37 +0200

    Working prediction model

--- 04_Signature_Verification/src/distances.py
+++ 04_Signature_Verification/src/distances.py
@@ -1,18 +1,41 @@
 import numpy as np
 from dtaidistance import dtw_ndim
 
+def calculate_distance(signature_test, signatures_real) -> np.array:
+    """
+    Calculate the distances between a signature and and array of signatures
 
-def mean_distance(signatures):
-    # Join features together e.g. (x, y) OR (x, y, pressure)
-    signature_features = [np.stack((sig.normalise().x, sig.normalise().y), axis=1) for sig in signatures]
-    signature_features = np.asarray(signature_features)
+    :return: Array of distance to each genuine signature  np.array
+    """
+    num_signatures = len(signatures_real)
+    distances = np.zeros((num_signatures, 1))
 
-    # Calculate distance matrix NOTE: unsure if axis is 1 OR 0
-    distance_matrix = dtw_ndim.distance_matrix(signature_features, 1)
+    # join signature features e.g (x, y) or (x, y, vy, vx) returned a list of feature arrays
+    signature_features = [
+        np.stack((sig.normalise().x, sig.normalise().y), axis=1) for sig in signatures_real
+    ]
+    test_features = np.stack((signature_test.normalise().x, signature_test.normalise().y), axis=1)
 
-    # Flatten and remove duplicate distances
-    #  0, 1, 2
-    #  1, 0, 3  = [1,2,3]
-    #  2, 3, 0
-    distances = distance_matrix[np.triu_indices(len(distance_matrix), k=1)]
-    return np.mean(distances)
+    # Calculate test signature against true signatures
+    for i, sig in enumerate(signature_features):
+        distances[i] = dtw_ndim.distance_fast(test_features, sig)
+
+    return distances
+
+
+def predict_fake(unverified_signatures, real_signatures, threshold=4):
+    # compare signature against all genuine
+    distances = calculate_distance(unverified_signatures, real_signatures)
+
+    distances_mean = np.mean(distances)
+
+    # classify signature as genuine if less the threshold set
+    prediction = False if distances_mean < threshold else True
+
+    # compare our prediction with ground truth
+    correct_prediction = prediction == unverified_signatures.is_fake
+
+    # print each comparison
+    # print('GT = {0}, Predicted = {1}'.format(unverified_signatures.is_fake, prediction))
+
+    return correct_prediction
--- 04_Signature_Verification/src/main.py
+++ 04_Signature_Verification/src/main.py
@@ -1,10 +1,22 @@
-import distances
+from distances import *
 from users import *
 
+# signature to test whether fake or genuine
+test = read_verification_users()
 users = read_enrollment_users()
+num_users = len(users)
 
-user = users[1]  # Select user 001
-signatures = user.signatures  # user 001 Real signatures
+# compare all users test signatures against genuine
+for i in range(1, num_users):
+    signatures_real = users[i].signatures
+    signatures_test = test[i].signatures
 
-# Calculate the average distance
-distances.mean_distance(signatures)
+    correct = 0
+    for test_sig in signatures_test:
+        prediction = predict_fake(test_sig, signatures_real, threshold=4)
+        if prediction:
+            correct += 1
+
+    accuracy = (correct / len(signatures_test)) * 100
+    print('User: {0}, Average Accuracy: {1:.2f}%'.format(i, accuracy))
+
